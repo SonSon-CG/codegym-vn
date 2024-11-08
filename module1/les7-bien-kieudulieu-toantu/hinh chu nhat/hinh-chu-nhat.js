948d72305183fd21b0fc9153d1611bc4088c03ea
SonSon-CG <son.mup@gmail.com>
2024-11-08 11:31:01 +0700

    updateeee

--- module1/les7-bien-kieudulieu-toantu/hinh chu nhat/hinh-chu-nhat.js
+++ module1/les7-bien-kieudulieu-toantu/hinh chu nhat/hinh-chu-nhat.js
@@ -7,5 +7,5 @@ inputHeight = prompt("Enter the height");
 
 // let width = parseInt(inputWidth);
 // // let height = parseInt(inputHeight);
-// let area = inputWidth * inputHeight;
-// document.write("The area is: " + area);
+area = inputWidth * inputHeight;
+document.write("The area is: " + area);
